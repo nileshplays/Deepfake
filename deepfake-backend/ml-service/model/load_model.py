ca49b4580aeacea2002a44fd35d2d1bf18bbc688
nileshplays <nileshsarkar7731@gmail.com>
2026-02-13 15:08:12 +0530

    Load model with compile=False for compatibility

--- deepfake-backend/ml-service/model/load_model.py
+++ deepfake-backend/ml-service/model/load_model.py
@@ -2,9 +2,11 @@ import tensorflow as tf
 
 def load_model():
     """
-    Loads the trained deepfake detection model (.h5)
-    This runs ONCE when FastAPI starts.
+    Loads trained deepfake model safely (Render compatible)
     """
-    model = tf.keras.models.load_model("model/deepfake_model.h5")
+    model = tf.keras.models.load_model(
+        "model/deepfake_model.h5",
+        compile=False
+    )
     print("✅ Real deepfake model loaded successfully")
     return model
