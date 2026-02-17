c9de567b0b9ab5ecf60de5e5ce17a52a57dc1b6a
nileshplays <nileshsarkar7731@gmail.com>
2026-02-17 14:26:00 +0530

    Fix 2

--- deepfake-backend/ml-service/model/load_model.py
+++ deepfake-backend/ml-service/model/load_model.py
@@ -5,7 +5,7 @@ def load_model():
     Loads trained deepfake model safely (Render compatible)
     """
     model = tf.keras.models.load_model(
-        "model/deepfake_render_ready.h5",
+        "model/deepfake_model.h5",
         compile=False
     )
     print("✅ Real deepfake model loaded successfully")
